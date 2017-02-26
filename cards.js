1f1d1a1ca091411d6d338a4640465fd267ee4437
webbdm <gwebb762@gmail.com>
2017-02-26 15:54:37 -0600

    added button

---
+++ cards.js
@@ -0,0 +1,10 @@
+var textInput = document.getElementById('textInput');
+var button = document.getElementById('button');
+
+// var textInput = e.value;
+
+button.addEventListener("click", function(){
+
+	console.log("button was clicked", textInput.value);
+})
+
