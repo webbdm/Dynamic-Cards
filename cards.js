0697a2cec30f5ba64346ebf8040bf58c5e76add8
webbdm <gwebb762@gmail.com>
2017-02-26 17:29:19 -0600

    added dynamic  card

--- cards.js
+++ cards.js
@@ -1,10 +1,24 @@
 var textInput = document.getElementById('textInput');
 var button = document.getElementById('button');
-
+var cardholder = document.getElementById('cardholder');
 // var textInput = e.value;
 
-button.addEventListener("click", function(){
 
-	console.log("button was clicked", textInput.value);
-})
+
+
+
+button.addEventListener("click", function writeToDom(){
+	
+	var newCard="";
+	newCard += `<div class="cardBox">`
+	newCard += `<h1>${textInput.value}</h1>`
+	newCard += `<button class="button" id="delete">DELETE</button>`
+	newCard += `</div>`
+	cardHolder.innerHTML += newCard;
+
+});
+
+
+	
+
 
